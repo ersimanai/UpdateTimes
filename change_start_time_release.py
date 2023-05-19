b73bc03b3a2bfa6d94f66efa8f687c8a688646b4
yangxiaojun <xiaojun.yang@capstonegaming.com>
2023-05-19 17:12:06 +0800

    update

--- change_start_time_release.py
+++ change_start_time_release.py
@@ -6,6 +6,54 @@ import requests
 import subprocess
 import getpass
 
+#体验机会
+def free():
+    # 最大使用次数
+    max_usage_count = 3
+    # 记录当前使用次数的文件路径
+    usage_file = os.path.expanduser("~/.usagelog")
+    
+    # 检查文件是否存在
+    if os.path.exists(usage_file):
+        # 读取当前使用次数
+        try:
+            with open(usage_file, "r") as f:
+                usage_count = int(f.read())
+        except ValueError:
+            # 如果文件内容无法转换为整数，则使用默认值 0
+            usage_count = 0
+    else:
+        # 文件不存在时，初始化为 0
+        usage_count = 0
+    
+    # 判断是否达到使用次数上限
+    if usage_count >= max_usage_count:
+        print("免费次数用完了。")
+        return False
+    
+    # 更新使用次数
+    usage_count += 1
+    
+    # 保存当前使用次数
+    with open(usage_file, "w") as f:
+        f.write(str(usage_count))
+    
+    return True
+
+
+
+# 获取本机 MAC 地址和用户名
+mac_id = None
+def get_mac_serial_number():
+    output = subprocess.check_output(['system_profiler', 'SPHardwareDataType'])
+    lines = output.splitlines()
+    for line in lines:
+        line = line.strip()
+        if line.startswith(b"Serial Number (system):"):
+            return line.split(b":")[1].strip().decode("utf-8")
+
+mac_id = get_mac_serial_number()
+
 # 检查标志，记录是否已经输入正确的激活码
 activation_done = False
 
@@ -14,60 +62,49 @@ activation_file = os.path.expanduser('~/.activation_flag')
 if os.path.exists(activation_file):
     activation_done = True
 
-# 获取当前时间戳
-current_time = int(time.time())
-
 # 获取截止时间戳
 def get_deadline():
+    # 获取当前时间戳
+    current_time = int(time.time())
     response = requests.get("http://101.35.227.215:8008/get_timestamp")
     if response.status_code == 200:
-        return response.json().get('timestamp')
+        deadline =  response.json().get('timestamp')
     else:
         # 如果无法连接到服务器或获取失败，返回默认的deadline值
-        return current_time
+        deadline = current_time
+    if current_time < deadline:
+        return True
+    return False
+
+#根据macId查看是否已经绑定了激活码
+def get_bind_mac(mac_id):
+    data = {
+        'mac_id' : mac_id
+    }
+    response = requests.post("http://101.35.227.215:8008/get_bind_mac", data=data)
+    if response.status_code == 200:
+        result = response.json().get('result')
+        return result
+    return False;
+
+isFree = free()
+
+#设备绑定了激活码或者免激活码期间activation_done为true
 if not activation_done:
     deadline = get_deadline()
-    # 判断是否在指定时间截止之前
-    if current_time < deadline:
+    bindmac = get_bind_mac(mac_id)
+    if deadline or bindmac:
         activation_done = True
         with open(activation_file, "w") as f:
                 f.write("activation done")
-                
-if not activation_done:
+
+
+if not activation_done and not isFree:
     activation_code = input("请输入激活码: ")
-
-    # 获取本机 MAC 地址和用户名
-    mac_address = None
-    username = None
-
-    if sys.platform.startswith('win'):
-        output = subprocess.getoutput('ipconfig /all')
-        for line in output.splitlines():
-            if 'Physical Address' in line:
-                mac_address = line.split(':')[1].strip()
-                break
-    else:
-        output = subprocess.getoutput('ifconfig')
-        for line in output.splitlines():
-            if 'ether' in line:
-                mac_address = line.split()[1]
-                break
-
-    if mac_address:
-        try:
-            username = getpass.getuser()
-        except Exception:
-            pass
-
-    if not mac_address or not username:
-        print("无法获取 MAC 地址或用户名")
-        sys.exit()
-
     # 构建请求数据
     data = {
         'code': activation_code,
-        'mac': mac_address,
-        'mac_name': username
+        'mac_id': mac_id
     }
 
     # 向服务器发送 POST 请求验证激活码使用信息
@@ -76,6 +113,7 @@ if not activation_done:
     if response.status_code == 200:
         result = response.json().get('result')
         if result == 0:
+            activation_done = True
             with open(activation_file, "w") as f:
                 f.write("activation done")
             usage_count = response.json().get('usage_count')
@@ -91,8 +129,8 @@ if not activation_done:
             for record in usage_records:
                 usage_count = record['usage_count']
                 timestamp = record['timestamp']
-                mac_name = record['mac_name']
-                print(f"使用次数: {usage_count}, 时间: {timestamp}, MAC 名称: {mac_name}")
+                mac_name = record['mac_id']
+                print(f"使用激活码时间: {timestamp}, MAC 序列号: {mac_name}")
             sys.exit()
         else:
             print("未知错误")
